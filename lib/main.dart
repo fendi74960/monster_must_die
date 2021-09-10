5853b2b11bdd1fa3502e04f6b8650fa8c9ef22b9
fendi74960 <serpiente61@hotmail.fr>
2021-09-10 21:05:51 +0200

    yo

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,86 @@
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  final myGame = MyGame();
+
+  runApp(
+    GameWidget(
+      game: myGame,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyGame extends Game {
+  // A constant speed, represented in logical pixels per second
+  static const int squareSpeed = 400;
 
-  // This widget is the root of your application.
+  // To represent our square we are using the Rect class from dart:ui
+  // which is a handy class to represent this type of data. We will be
+  // seeing other types of data classes in the future, but for this
+  // example, Rect will do fine for us.
+  late Rect squarePos;
+
+  // To represent our direction, we will be using an int value, where 1 means
+  // going to the right, and -1 going to the left, this may seems like a too much
+  // simple way of representing a direction, and indeed it is, but this will
+  // will work fine for our small example and will make more sense when we implement
+  // the update method
+  int squareDirection = 1;
+
+  // The onLoad method is where all of the game initialization is supposed to go
+  // For this example, you may think that this square could just be initialized on the field
+  // declaration, and you are right, but for learning purposes and to present the life cycle method
+  // for this example we will be initializing this field here.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  Future<void> onLoad() async {
+    Flame.device.fullScreen();
+    squarePos = Rect.fromLTWH(-20, -50, 100, 100);
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  // BasicPalette is a help class from Flame, which provides default, pre-built instances
+  // of Paint that can be used by your game
+  static final squarePaint = BasicPalette.white.paint;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void update(double dt) {
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // Here we move our square by calculating our movement using
+    // the iteration delta time and our speed variable and direction.
+    // Note that the Rect class is immutable and the translate method returns a new Rect instance
+    // for us, so we just re-assign it to our square variable.
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    // It is important to remember that the result of the execution of this method,
+    // must be the game state (in our case our rect, speed and direction variables) updated to be
+    // consistent of how it should be after the amount of time stored on the dt variable,
+    // that way your game will always run smooth and consistent even when a FPS drop or peak happen.
+    //
+    // To illustrate this, if our square moves at 200 logical pixels per second, and half a second
+    // has passed, our square should have moved 100 logical pixels on this iteration
+    squarePos = squarePos.translate(squareSpeed * squareDirection * dt, 0);
+
+    // This simple condition verifies if the square is going right, and has reached the end of the
+    // screen and if so, we invert the direction.
+    //
+    // Note here that we have used the variable size, which is a variable provided
+    // by the Game class which contains the size in logical pixels that the game is currently using.
+    if (squareDirection == 1 && squarePos.right > size.x) {
+      squareDirection = -1;
+      // This does the same, but now checking the left direction
+    } else if (squareDirection == -1 && squarePos.left < 0) {
+      squareDirection = 1;
+    }
+  }
+
+
+
+
+  @override
+  void render(Canvas canvas) {
+    // Canvas is a class from dart:ui and is it responsible for all the rendering inside of Flame
+    canvas.drawRect(squarePos, squarePaint());
   }
 }
+
