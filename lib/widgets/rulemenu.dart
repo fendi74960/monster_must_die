2248fc1a71d1f0b4f9edcb9ebff2abf5e8239edc
BaubYo <66359241+BaubYo@users.noreply.github.com>
2021-11-09 11:20:49 +0100

    Finalisation du menu rule

--- lib/widgets/rulemenu.dart
+++ lib/widgets/rulemenu.dart
@@ -1,9 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:monster_must_die/controller/bestiarycontroller.dart';
 import 'package:monster_must_die/games/gamesetting.dart';
-import 'package:monster_must_die/widgets/hud.dart';
-import 'package:provider/provider.dart';
+import 'package:monster_must_die/widgets/main_menu.dart';
 
 class RuleMenu extends StatefulWidget {
   // An unique identified for this overlay.
@@ -43,33 +41,45 @@ class _RuleMenu extends State<RuleMenu> {
                         padding: const EdgeInsets.symmetric(
                             vertical: 20, horizontal: 100),
                         child: Container(
-                          child: ListView(
-                              children: [
+                          child: Column(children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                gameRef.overlays.remove(RuleMenu.id);
+                                gameRef.overlays.add(MainMenu.id);
+                              },
+                              child: Text(
+                                'Go back',
+                                style: TextStyle(
+                                  fontSize: textSizeDefault * 50,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: ListView(children: [
                                 Text(
-                                  RuleText.getText(),
+                                  RuleText.getTextEN(),
                                   style: TextStyle(
-                                    fontSize: 30,
+                                    fontSize: textSizeDefault * 30,
+                                    color: Colors.white,
                                   ),
                                 ),
-                                Text(
-                                  RuleText.getText(),
-                                  style: TextStyle(
-                                    fontSize: 30,
-                                  ),
-                                ),
-                              ]
-                          ),
-                            height: MediaQuery.of(context).size.height* 0.7,
-                            width: MediaQuery.of(context).size.width,
-                              )
-                            )
-                        )
-                    )));
+                              ]),
+                              height: MediaQuery.of(context).size.height * 0.7,
+                              width: MediaQuery.of(context).size.width,
+                            ),
+                          ]),
+                          height: MediaQuery.of(context).size.height * 0.8,
+                          width: MediaQuery.of(context).size.width,
+                        ))))));
   }
 }
 
 class RuleText {
-  static String getText() {
-    return " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis posuere odio non viverra. Ut euismod leo pretium metus scelerisque, id vestibulum nulla malesuada. Duis iaculis aliquam lacus, tincidunt porta lorem ultrices mattis. Nullam ultricies vel sem ut pretium. Praesent eu sapien lectus. Nulla ultricies suscipit turpis, nec imperdiet velit lacinia vel. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce quis varius enim. Donec orci justo, porttitor non neque vitae, blandit ornare mi. Vivamus ut ipsum congue, malesuada arcu vel, congue dui.     Integer blandit pellentesque est et convallis. Donec luctus ac sem sed finibus. Nullam interdum nibh quam, vel porta ipsum molestie quis. Proin non malesuada sapien, in feugiat ligula. Pellentesque pretium, urna id euismod euismod, eros magna ultricies leo, at semper nisi erat eget lectus. Aenean ut fringilla urna. Nulla consectetur, nibh sit amet dignissim aliquet, risus nibh mollis neque, ut pharetra nibh turpis nec sapien. Sed tempus erat et turpis elementum, vitae efficitur metus pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In eu sapien ac ligula vulputate lobortis nec et lectus. Cras blandit velit eu quam dignissim, mollis lacinia lorem tincidunt.";
+  static String getTextEN() {
+    return "\nWelcome adventurer!\nCome with us to pass this test.\nYou are going to face an army of monsters, each with their own speciality.\nFortunately, you and your teammate have the possibility to call units or to cast spells. Be careful, this uses score points.\nBut some types of enemies are resistant, or even invulnerable to your abilities, so ask your ally for help (either by speaking directly or by clicking on an enemy).\nTo learn more about these monsters, you can consult the bestiary (after logging in).\nThe goal of this game is to survive the waves of enemies as long as possible.\nTo play this game you need a Dart server (Socket IO) that runs on the port 3000 preferably locally to be able to use the \"Auto-connect\" button.\nPS : the function to track what the players are doing requires that the players are under android.";
+  }
+
+  static String getTextFR() {
+    return "\nBienvenu aventurier!\nViens accompagné pour réussir cette épreuve.\nVous allez faire face a une armée de monstre, qui ont chacun leur spécialité.\nHeureusement, toi et ton coéquipier, vous avez la possibilité d’appeler des unités ou encore de jeter des sorts. Attention cela utilise des points de score.\nMais certain type d’ennemies sont résistants, voire invulnérable face a tes capacités, donc demande de l’aide à ton allié (soit directement a l’oral, soit en cliquant sur un ennemi).\nPour en apprendre plus sur ces monstres, vous pouvez consulter le bestiaire (après vous être connecté).\nLe but de ce jeu est de survivre le plus longtemps aux vagues d’ennemies.\nPour fonctionner ce jeu nécessite un serveur Dart (Socket IO) qui fonctionne sur le port 3000 en local de préférence pour pouvoir utiliser le bouton \"Auto-connect\".\nPS : la fonction pour traquer ce que les joueurs font nécessite à ce que les joueurs soit sous android.";
   }
 }
